df66adcaa77d2f1b55acb9db2e0da1a58be78917
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-04-02 09:04:48 +0300

    copied state

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      justCopied: false
+    };
+  }
+
   componentDidMount() {
     this.clipboard = new Clipboard(this.refs.copy);
 
     this.clipboard.on('success', () => {
-      alert('it works');
+      this.setState({ justCopied: true });
+      setTimeout(() => this.setState({ justCopied: false }), 1000);
     }).on('error', () => {
       alert('Unable to copy. Please manually copy the link');
     });
@@ -21,7 +29,9 @@ export default class LinksListItem extends React.Component {
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
-        <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+          {this.state.justCopied ? 'Copied' : 'Copy'}
+        </button>
       </div>
     );
   }
