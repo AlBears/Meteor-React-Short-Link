2dc3edb6e5ed87c60a514e604508c45ca3ac28cb
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-04-03 09:08:00 +0300

    controlled input

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,22 +2,41 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor'
 
 export default class AddLink extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      url: ''
+    }
+  }
   onSubmit(e) {
-    const url = this.refs.url.value.trim();
+    const { url } = this.state;
 
     e.preventDefault();
 
     if (url) {
-      Meteor.call('links.insert', url);
+      Meteor.call('links.insert', url, (err, res) => {
+        if (!err) {
+          this.setState({ url: '' });
+        }
+      });
       this.refs.url.value = '';
     }
+  }
+  onChange(e) {
+    this.setState({
+      url: e.target.value
+    });
   }
   render() {
     return (
       <div>
         <p>Add Link</p>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL"/>
+          <input
+            type="text"
+            value={this.state.url}
+            placeholder="URL"
+            onChange={this.onChange.bind(this)}/>
           <button>Add Link</button>
         </form>
       </div>
