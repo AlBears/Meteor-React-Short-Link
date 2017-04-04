b750b2ab0239f969984e93209fa194fd2711da0f
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-04-04 15:40:20 +0300

    styling modal

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -44,19 +44,22 @@ export default class AddLink extends React.Component {
           isOpen={this.state.isOpen}
           contentLabel="Add Link"
           onAfterOpen={() => this.refs.url.focus()}
-          onRequestClose={ this.handleModalClose.bind(this) }>
+          onRequestClose={ this.handleModalClose.bind(this) }
+          className="boxed-view__box"
+          overlayClassName="boxed-view boxed-view--modal">
           <h1>Add Link</h1>
           {this.state.error ? <p>{this.state.error}</p> : undefined }
-          <form onSubmit={this.onSubmit.bind(this)}>
+          <form className="boxed-view__form" onSubmit={this.onSubmit.bind(this)}>
             <input
               type="text"
               value={this.state.url}
               placeholder="URL"
               ref="url"
               onChange={this.onChange.bind(this)}/>
-            <button>Add Link</button>
+            <button className="button">Add Link</button>
+            <button type="button" className="button button--secondary" onClick={ this.handleModalClose.bind(this) }>Cancel</button>
           </form>
-          <button onClick={ this.handleModalClose.bind(this) }>Cancel</button>
+
         </Modal>
       </div>
     )
